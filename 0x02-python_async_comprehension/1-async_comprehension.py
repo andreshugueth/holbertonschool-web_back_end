7d3842e275dcf3240a3698022c4ea090e0af968f
andreshugueth <andreshugueth@gmail.com>
2021-07-25 23:57:06 -0500

    0x02 python async comprehension task 1 pep8 fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """coroutine will collect 10 random numbers using an async comprehensing over async_generator
+    """coroutine will collect 10 random numbers using an
+    async comprehensing over async_generator
     Returns:
         List[float]: return the 10 random numbers
     """
