c74bb8a0397b2b374cb339411377af5cb967d33f
andreshugueth <andreshugueth@gmail.com>
2021-07-26 22:42:45 -0500

    0x01 python async task 2 fix return type

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -15,7 +15,8 @@ async def measure_time(n: int, max_delay: int) -> float:
     Returns:
         float: returns total_time / n
     """
-    t1 = time.perf_counter()
+    elapsed_time: float
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    t2 = time.perf_counter()
-    return (t2 - t1) / n
+    elapsed_time = time.perf_counter() - start_time
+    return elapsed_time / n
