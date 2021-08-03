f3fd17e2fc9e79df7c1ccf943e5ebb115f2f7394
andreshugueth <andreshugueth@gmail.com>
2021-08-02 21:14:22 -0500

    0x05 task 3-6

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 import logging
+import mysql.connector
+from os import getenv
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -66,3 +68,43 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Redacting Formatter class."""
+    db = mysql.connector.connection.MySQLConnection(
+        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME'))
+
+    return db
+
+
+def main():
+    """ Redacting Formatter class."""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    result = cursor.fetchall()
+    for data in result:
+        message = f"name={data[0]}; " + \
+                  f"email={data[1]}; " + \
+                  f"phone={data[2]}; " + \
+                  f"ssn={data[3]}; " + \
+                  f"password={data[4]}; " + \
+                  f"ip={data[5]}; " + \
+                  f"last_login={data[6]}; " + \
+                  f"user_agent={data[7]};"
+        print(message)
+        log_record = logging.LogRecord("my_logger", logging.INFO,
+                                       None, None, message, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(log_record)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
