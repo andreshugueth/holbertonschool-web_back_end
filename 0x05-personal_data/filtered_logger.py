fd7deb4ee1de3c62c11b37d68a9ab0f73e282a24
andreshugueth <andreshugueth@gmail.com>
2021-08-02 21:07:41 -0500

    0x05 personal data task 1-2

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,35 @@
 """Regex-ing"""
 import re
 from typing import List
+import logging
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format method to class
+
+        Args:
+            record (logging.LogRecord): [description]
+
+        Returns:
+            str: [description]
+        """
+        msg = filter_datum(self.fields, self.REDACTION,
+                           super().format(record), self.SEPARATOR)
+        return msg
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -21,3 +50,19 @@ def filter_datum(fields: List[str], redaction: str,
                          f"{field}={redaction}{separator}",
                          message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """get_logger implementation
+
+    Returns:
+        logging.Logger:
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
