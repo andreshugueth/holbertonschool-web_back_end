41674cef083ba06b6bdece13ab1428277433dca2
andreshugueth <andreshugueth@gmail.com>
2021-08-07 22:01:09 -0500

    0x06 basic auth task 8-9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 import re
 
 
@@ -27,3 +28,39 @@ class BasicAuth(Auth):
         header = authorization_header.split(' ')
         return (None if not bool(re.search('^Basic ', authorization_header))
                 else header[1])
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+
+        Args:
+            base64_authorization_header (str): base64 auth header
+
+        Returns:
+            str: decoded value of a base64 string
+        """
+        if (not base64_authorization_header or
+                type(base64_authorization_header) != str):
+            return None
+        try:
+            msg = base64.b64decode(base64_authorization_header.encode('utf-8'))
+            return msg.decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """[summary]
+
+        Args:
+            self ([type]): [description]
+            str ([type]): [description]
+        """
+        if (not decoded_base64_authorization_header
+                or type(decoded_base64_authorization_header) != str):
+            return (None, None)
+        credendials = decoded_base64_authorization_header.split(':', 1)
+        return (credendials[0], credendials[1]) if ":" in\
+            decoded_base64_authorization_header else (None, None)
