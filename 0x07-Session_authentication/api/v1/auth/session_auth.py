9ad4586d686b954b1a945280ee43dab9717e1840
andreshugueth <andreshugueth@gmail.com>
2021-08-10 23:05:45 -0500

    0x07 session auth task 2

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module of Session Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,20 @@ class SessionAuth(Auth):
     Args:
         Auth ([type]): Inherits from Auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session method
+
+        Args:
+            user_id (str, optional): [description]. Defaults to None.
+
+        Returns:
+            str: - Return None if user_id is None
+                 - Return None if user_id is not a string
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
