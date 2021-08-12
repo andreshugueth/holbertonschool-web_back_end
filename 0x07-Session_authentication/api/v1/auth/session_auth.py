4899faa46587d7cd848865df2973d67a1ae56ba0
andreshugueth <andreshugueth@gmail.com>
2021-08-11 23:14:40 -0500

    0x07 session auth task 6

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -41,3 +42,15 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """[summary]
+
+        Args:
+            request ([type], optional): [description]. Defaults to None.
+        """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
