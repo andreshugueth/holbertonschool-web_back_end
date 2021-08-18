710146c8050db0795f3a94a0bfbfd26aea59a2c3
andreshugueth <andreshugueth@gmail.com>
2021-08-18 17:32:59 -0500

    0x08 user auth service task 1 docstring

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-DB model
-"""
+"""DB model"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,8 +9,7 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance
@@ -24,8 +21,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
