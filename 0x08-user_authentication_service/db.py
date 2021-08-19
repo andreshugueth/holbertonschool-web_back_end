5560208465a30c16b798ee7bbfe1b0d566b99562
andreshugueth <andreshugueth@gmail.com>
2021-08-18 21:46:00 -0500

    0x08 user auth service task 2

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -40,4 +42,17 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user by attribute
+
+        Returns:
+            User: Returns the user if succeed
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).one()
+        if not user:
+            raise NoResultFound
         return user
