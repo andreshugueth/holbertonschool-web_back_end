c44e6cc43d9a6b755208d80d60e22d8146f81539
andreshugueth <andreshugueth@gmail.com>
2021-08-18 22:54:14 -0500

    0x08 user auth service task 6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Flask app"""
+from flask import Flask, make_response, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home():
+    """Home page"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
