83525c600959acd2d4ace0d4c907beed9f11a91a
andreshugueth <andreshugueth@gmail.com>
2021-08-18 23:34:56 -0500

    0x08 user auth service task 8

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -42,3 +42,20 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Valid login method
+
+        Args:
+            email (str): email credential
+            password (str): password credential
+
+        Returns:
+            bool: True or False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return (True if bcrypt.checkpw(password.encode(), user.hashed_password)
+                else False)
