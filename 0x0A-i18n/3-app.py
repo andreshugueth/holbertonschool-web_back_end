bbf9f323f258a7f2bbe8f376c418b3eac166a22d
andreshugueth <andreshugueth@gmail.com>
2021-08-26 23:39:32 -0500

    0x0A i18n task 3

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-""" 2-app module """
+""" 3-app module """
+from typing import Union
 from flask import Flask, request
 from flask_babel import Babel
-from routes.routes_2 import app_routes
+from routes.routes_3 import app_routes
+from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
-app.config.from_object('1-app.Config')
+app.config.from_object(Config)
 app.register_blueprint(app_routes)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """ Determine best match for supported languages
+def get_locale() -> Union[str, None]:
+    """ get locale
     """
-    return request.accept_languages.best_match(['en', 'fr'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
