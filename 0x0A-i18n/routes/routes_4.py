bd37e87f27d5c25c4c021fd84fe2187d0041f5d7
andreshugueth <andreshugueth@gmail.com>
2021-08-26 23:55:01 -0500

    0x0A i18n task 4

--- 0x0A-i18n/routes/routes_4.py
+++ 0x0A-i18n/routes/routes_4.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic route task 3"""
+"""Basic route task 4"""
 from flask import Blueprint, render_template
 
 
@@ -9,4 +9,4 @@ app_routes = Blueprint('app_routes', __name__)
 @app_routes.route('/', methods=["GET"], strict_slashes=False)
 def home():
     """ Home page """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
