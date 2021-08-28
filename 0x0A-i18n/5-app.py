115ca38583b3502ebea2b435c383f496ee81785d
andreshugueth <andreshugueth@gmail.com>
2021-08-27 21:53:10 -0500

    0x0A i18n task 5 not showing name

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ 4-app module """
-from flask import Flask, request, g
+from flask import Flask, request
 from typing import Union
 from flask import Flask, request
 from flask_babel import Babel
@@ -12,23 +12,8 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 app.config.from_object(Config)
 app.register_blueprint(app_routes)
-
-
-def get_user():
-    """get user"""
-    try:
-        return users.get(int(request.args.get('login_as')))
-    except Exception:
-        return None
 
 
 @babel.localeselector
@@ -39,13 +24,6 @@ def get_locale() -> Union[str, None]:
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-@app.before_request
-def before_request():
-    """ before request
-    """
-    g.user = get_user()
 
 
 if __name__ == "__main__":
--- 0x0A-i18n/routes/routes_5.py
+++ 0x0A-i18n/routes/routes_5.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 """Basic route task 5"""
-from flask import Blueprint, render_template
+from typing import Union
+from flask import Blueprint, render_template, request, g
 
 
 app_routes = Blueprint('app_routes', __name__)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[dict, None]:
+    """get user"""
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
 
 
 @app_routes.route('/', methods=["GET"], strict_slashes=False)
 def home():
     """ Home page """
     return render_template('5-index.html')
+
+
+@app_routes.before_request
+def before_request():
+    """ before request
+    """
+    g.user = get_user()
