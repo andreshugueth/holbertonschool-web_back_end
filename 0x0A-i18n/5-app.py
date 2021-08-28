5838fa3e4394cf33dfa99728d081d183091a3637
andreshugueth <andreshugueth@gmail.com>
2021-08-27 22:09:29 -0500

    0x0A i18n task 5 not complete

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ 4-app module """
-from flask import Flask, request
+from flask import Flask, request, g
 from typing import Union
 from flask import Flask, request
 from flask_babel import Babel
@@ -11,6 +11,13 @@ from routes.routes_5 import app_routes
 app = Flask(__name__)
 babel = Babel(app)
 
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 app.config.from_object(Config)
 app.register_blueprint(app_routes)
@@ -24,6 +31,21 @@ def get_locale() -> Union[str, None]:
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+def get_user() -> Union[dict, None]:
+    """get user"""
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ before request
+    """
+    g.user = get_user()
 
 
 if __name__ == "__main__":
