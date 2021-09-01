63b50d3a01f48ec6a2efc801663f8a77a0eafb8a
andreshugueth <andreshugueth@gmail.com>
2021-09-01 13:34:48 -0500

    0x0B redis basic task 1

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Redis and Python exercise"""
+from typing import Union
+import uuid
+
+import redis
+
+
+class Cache():
+    """Cache class with redis"""
+
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store method
+
+        Args:
+            data (Union[str, bytes, int, float]): Data to be stored
+
+        Returns:
+            str: string
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
