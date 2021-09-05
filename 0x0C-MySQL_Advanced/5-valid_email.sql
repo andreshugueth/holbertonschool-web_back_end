2f1b6dcbff9960d3257c2b6b0995037fea66f427
andreshugueth <andreshugueth@gmail.com>
2021-09-04 19:08:45 -0500

    0x0C MySQL advanced task 5 fix trigger guard condition

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- SQL script that creates a trigger that resets the attribute
 -- valid_email only when the email has been changed
+DROP TRIGGER IF EXISTS reset_email;
 CREATE TRIGGER reset_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
